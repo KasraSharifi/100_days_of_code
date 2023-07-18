5eab78738f9fd6cf59919cd65eeb5d484b2651f0
Kasra Sharifi <sharifi.kasra72@gmail.com>
2023-07-17 22:15:35 -0500

    second commit of day 17

--- day17/quiz-game-start/quiz_brain.py
+++ day17/quiz-game-start/quiz_brain.py
@@ -1,3 +1,5 @@
+from data import question_data
+
 class QuizBrain():
     
     def __init__(self, question_list):
@@ -5,8 +7,24 @@ class QuizBrain():
         self.question_list = question_list
         
     
+    def do_we_still_have_questions(self):
+        """The purpose here is to make sure that we are not going above the number of questions
+        that we have"""
+        if self.question_number == len(self.question_list):
+                print("We have no more questions to ask")
+                return False
+        else:
+            return True
+        
+    
+    
     def nex_question(self):
-        current_question = self.question_list[self.question_number].text
-        current_answer = self.question_list[self.question_number].answer
-        self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question} True/False ")
+        ask_question = True
+        while ask_question == True:
+            current_question = self.question_list[self.question_number].text
+            current_answer = self.question_list[self.question_number].answer
+            self.question_number += 1
+            answer = input(f"Q.{self.question_number}: {current_question} True/False ")
+            
+            #Checking to make sure that we are not going above the number of questions that we have
+            ask_question = self.do_we_still_have_questions()
